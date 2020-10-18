956e8820ae03e0826a997bf75a99658b4f5fd7af
Mori 30R <ghahreman00@gmail.com>
2020-10-18 23:00:56 +0330

    adding theme.js

--- src/js/styles/theme.js
+++ src/js/styles/theme.js
@@ -3,3 +3,10 @@ export const lightText = '#FFFFFF';
 export const primaryColor = '#DCD4F5';
 export const primaryColorLight = '#E8E4FF';
 export const secondaryColor = '#FF74F1';
+export const thinText = '#B8B8B8';
+
+
+
+
+
+
